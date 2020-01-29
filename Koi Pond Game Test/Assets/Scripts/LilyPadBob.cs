ed2275b7ade2624b253ab81cb188bd8a1634a1ba
JackHill-SJ <hilljack1856@gmail.com>
2020-01-29 12:33:15 -0500

    Lillypad Rotation

---
+++ Koi Pond Game Test/Assets/Scripts/LilyPadBob.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Makes objects float up & down while gently spinning.
+public class LilyPadBob : MonoBehaviour
+{
+    // User Inputs
+    public float degreesPerSecond = 15.0f;
+
+    // Position Storage Variables
+    Vector3 posOffset = new Vector3();
+
+    // Use this for initialization
+    void Start()
+    {
+        // Store the starting position & rotation of the object
+        posOffset = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Spin object around Y-Axis
+        transform.Rotate(new Vector3(0f, Time.deltaTime * degreesPerSecond, 0f), Space.World);
+    }
+}
