fe52f42087d4368a8609eaa698cf054fa7d43530
JackHill-SJ <hilljack1856@gmail.com>
2020-02-18 10:21:51 -0500

    Updated Lilypad Movement

--- Koi Pond Game Test/Assets/Scripts/LilyPadMoveFromPlayer.cs
+++ Koi Pond Game Test/Assets/Scripts/LilyPadMoveFromPlayer.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class LilyPadMoveFromPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+    public float fleeDistance = 2.5f;
+    public float turnSpeed = 20.0f;
+    public float smoothTime = 1.5f;
+    public float speed = 5.0f;
+
+    private Vector3 relativePosition;
+
+    private void Awake()
     {
-        
+        player = GameObject.FindGameObjectWithTag("Player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        if (distance < fleeDistance)
+        {
+            relativePosition = player.position - transform.position;
+
+            transform.position = Vector3.MoveTowards(transform.position, player.position, -speed * Time.deltaTime);
+        }
+
+        if (transform.position.y > 90)
+            transform.position = new Vector3(transform.position.x, 90, transform.position.z);
+
     }
 }
