782a5b66db6e452b86bf527c17ed7ac4106033ea
JackHill-SJ <hilljack1856@gmail.com>
2020-02-19 13:44:46 -0500

    Water Sounds and Randomized Bird Sounds

---
+++ Koi Pond Game Test/Assets/Scripts/BirdSoundSpawn.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSoundSpawn : MonoBehaviour
+{
+
+    public GameObject soundObject;
+    [SerializeField] private Vector3 spawnValues;
+    private float spawnWait;
+    [SerializeField] private float spawnMostWait;
+    [SerializeField] private float spawnLeastWait;
+    public bool stopSpawning;
+
+    void Start()
+    {
+        StartCoroutine(Spawner());
+    }
+
+    void Update()
+    {
+        spawnWait = Random.Range(spawnLeastWait, spawnMostWait);
+    }
+
+    IEnumerator Spawner()
+    {
+        while (!stopSpawning)
+        {
+            Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), Random.Range(-spawnValues.y, spawnValues.y), Random.Range(-spawnValues.z, spawnValues.z));
+
+            Instantiate(soundObject, spawnPosition + transform.TransformPoint(0, 0, 0), gameObject.transform.rotation);
+
+            yield return new WaitForSeconds(spawnWait);
+        }
+    }
+}
---
+++ Koi Pond Game Test/Assets/Scripts/PlayRandomBirdSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayRandomBirdSound : MonoBehaviour
+{
+    public AudioClip[] birdSounds;
+
+    void Start()
+    {
+        int randSound = Random.Range(0, 9);
+        PlaySound(randSound);
+        StartCoroutine(DestroySelf());
+    }
+
+    void PlaySound (int clip)
+    {
+        AudioSource audio = GetComponent<AudioSource>();
+        audio.clip = birdSounds[clip];
+        audio.PlayOneShot(audio.clip, 0.8f);
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(4);
+        Destroy (gameObject);
+    }
+}
