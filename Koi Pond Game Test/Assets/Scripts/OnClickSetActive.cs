9107c3076ff13702fa19c420c5612e587eda85ff
JackHill-SJ <55606044+JackHill-SJ@users.noreply.github.com>
2020-10-06 17:44:00 -0700

    Broken Food Droplet Particle System

---
+++ Koi Pond Game Test/Assets/Scripts/OnClickSetActive.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClickSetActive : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        { 
+            gameObject.SetActive(true);
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
