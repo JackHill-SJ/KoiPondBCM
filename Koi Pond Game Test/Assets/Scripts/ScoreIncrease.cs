e1c561299c9a38face39230d4ace21c302897aea
hack2642 <56882186+hack2642@users.noreply.github.com>
2020-10-13 11:29:35 -0400

    Added a basic scoring system and score cubes

---
+++ Koi Pond Game Test/Assets/Scripts/ScoreIncrease.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreIncrease : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "fish")
+        {
+            ScoreManager.score += 100;
+            Debug.Log(ScoreManager.score);
+            Destroy(gameObject);
+        }
+    }
+}
