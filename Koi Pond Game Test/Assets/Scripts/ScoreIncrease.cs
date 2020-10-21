0cdb111f66c851309357bb3b6188cc5be97013de
hack2642 <56882186+hack2642@users.noreply.github.com>
2020-10-20 20:02:17 -0400

    Added Random Point Spawner

--- Koi Pond Game Test/Assets/Scripts/ScoreIncrease.cs
+++ Koi Pond Game Test/Assets/Scripts/ScoreIncrease.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class ScoreIncrease : MonoBehaviour
 {
+    public GameObject PointCube;
+
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "fish")
         {
             ScoreManager.score += 100;
             Debug.Log(ScoreManager.score);
+            SpawnNewCube();
             Destroy(gameObject);
         }
+    }
+
+    public void SpawnNewCube()
+    {
+        int randomIntX = Random.Range(-14,14);
+        int randomIntZ = Random.Range(-4, 11);
+
+        Instantiate(PointCube, new Vector3(randomIntX, -2.5f, randomIntZ), Quaternion.identity);
     }
 }
