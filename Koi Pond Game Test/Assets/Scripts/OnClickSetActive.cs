adf8bf95e9459d9f7571c0e99460146cca0d1e07
JackHill-SJ <55606044+JackHill-SJ@users.noreply.github.com>
2020-10-21 10:22:33 -0700

    Ver 2

--- Koi Pond Game Test/Assets/Scripts/OnClickSetActive.cs
+++ Koi Pond Game Test/Assets/Scripts/OnClickSetActive.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class OnClickSetActive : MonoBehaviour
 {
+    private ParticleSystem particles;
+    private void Start()
+    {
+        particles = GetComponent<ParticleSystem>();
+    }
+
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        { 
-            gameObject.SetActive(true);
+        if (Input.GetMouseButton(0))
+        {
+            particles.Play();
         }
         else
         {
-            gameObject.SetActive(false);
+            particles.Stop();
         }
     }
 }
